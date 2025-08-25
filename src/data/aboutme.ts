dfaf753dfbd8593473a3461a63cb2222fe540b37
SPARSH BATRA <sbatrabusiness@gmail.com>
2025-08-25 17:12:02 +0530

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -23,7 +23,7 @@ export const aboutMe: AboutMe = {
   institution: "Bhagwan Parshuram Institute of Technology, GGSIPU",
   // Note that links work in the description
   description:
-    "Hey! I'm a 21-year-old final-year IT student who gets excited about building things that actually work. I love playing around with data and seeing what stories it can tell—whether that's predicting if a tennis match will go my way or helping farmers figure out why their crops look sick. My projects are a mix of polished apps and messy experiments, but I'm always learning something new from each one.
+    "Hey! I'm a 21-year-old final-year IT student who gets excited about building things that actually work. I love playing around with data and seeing what stories it can tell—whether that's predicting if a tennis match will go my way or helping farmers figure out why their crops look sick. My projects are a mix of polished apps and messy experiments, but I'm always learning something new from each one."
 <br><br> "I'm really into ML and AI, especially when I can make complex stuff feel simple and useful. I'm always curious about what other people are building and love connecting with folks who share this passion. Right now I'm just looking for opportunities to grow, learn from people who know more than me, and maybe build something cool together.",
   
     cvUrl: "https://www.dropbox.com/scl/fi/i768lg5b8464s8re8nk24/SPARSH-BATRA_Resume.pdf?rlkey=n6jeuzqatjlbwcwkrjk1rpbf6&st=xzvp63np&dl=0",
